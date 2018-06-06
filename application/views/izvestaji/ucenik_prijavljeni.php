25e1db769d692eef84838cec374dc5df6e30b27c
Dragoljub Djordjevic <dragoljubd@gmail.com>
2018-06-06 13:19:13 +0200

    a

--- application/views/izvestaji/ucenik_prijavljeni.php
+++ application/views/izvestaji/ucenik_prijavljeni.php
@@ -3,8 +3,18 @@
 
 $ispiti=$_SESSION['prijava_ucenik'];
 
+//var_dump($ispiti);
+
 foreach ($ispiti as $red) {
-    echo $red['naziv_predmet']." ".$red['ime']." ".$red['prezime']."<br>";
+    echo $red['naziv_predmet']." ".$red['ime']." ".$red['prezime'];
+    foreach ($_SESSION['rok'] as $rok) {
+           if ($rok['idtip_roka']==$red['rok_idtip_roka']) {
+                     
+            echo " ".$rok['naziv'];
+                      
+            echo '<br>';
+           }
+}
 }
 
 
