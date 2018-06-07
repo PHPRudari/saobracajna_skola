9b77ad2e40744fd962f974b4ebd6f3e9b4062004
Dragoljub Djordjevic <dragoljubd@gmail.com>
2018-06-07 11:15:33 +0200

    aa

--- application/views/izvestaji/pregled_prijava.php
+++ application/views/izvestaji/pregled_prijava.php
@@ -1,9 +1,23 @@
 
-<a  href="<?php echo site_url($controller . "/stampa/stampa_prijava") ?>">Штампај пријављене испите</a>
-    <?php
 
+<div class="row">
+    <div class="col-md-6">
+        <h4> Сприсак пријављених испита за <?php
+            foreach ($_SESSION['rok'] as $rok) {
+                if ($rok['idtip_roka'] == $_POST['rok_prijave']) {
+
+                    echo $rok['naziv'];
+                }
+            }
+            ?>  рок </h4>
+    </div>
+    <div class="col-md-6"> 
+        <a class="dugme2" href="<?php echo site_url($controller . "/stampa/stampa_prijava") ?>">Штампај пријављене испите</a><br>
+    </div>
+</div>    
+<?php
 //var_dump($_SESSION['rok']);
-//var_dump($_SESSION['prijavljeni']);
+//var_dump($_SESSION['pregled_prijava']);
 
 
 
@@ -15,31 +29,41 @@ echo "<th>Ime </th>";
 echo "<th>Naziv predmeta</th>";
 echo "<th>Godina</th>";
 echo "</tr>";
-$i=1;
+$i = 1;
 
 if ($_SESSION['pregled_prijava'] == NULL) {
-    echo 'Nema prijavljenih ispita u ovom roku';
-}
+    echo '<td>Nema prijavljenih ispita u ovom roku</td>';
+} else
+    foreach ($_SESSION['pregled_prijava'] as $row) {
 
-else
-
-
-foreach ($_SESSION['pregled_prijava'] as $row) {
-    
-    echo $i.".".$row['naziv_predmet'].' '.$row['ime'].' '.$row['prezime'];
-   
-       foreach ($_SESSION['rok'] as $rok) {
-           if ($rok['idtip_roka']==$row['rok_idtip_roka']) {
-                     
-           // echo " ".$rok['naziv'];
-                      
-            echo '<br>';
-    $i++;
-       }    
-}
-    
-}
+        echo "<tr>";
+        echo "<td>" . $row['jedinstveni_broj_ucenik'] . "</td>";
+        echo "<td>" . $row['prezime'] . "</td>";
+        echo "<td>" . $row['ime'] . "</td>";
+        echo "<td>" . $row['naziv_predmet'] . "</td>";
+        echo "<td>" . $row['godina_obrazovanja_idgodina_obrazovanja'] . "</td>";
 
 
+
+
+
+
+
+
+
+
+
+
+
+        /*   foreach ($_SESSION['rok'] as $rok) {
+          if ($rok['idtip_roka']==$row['rok_idtip_roka']) {
+
+          // echo " ".$rok['naziv'];
+
+          echo '<br>';
+          $i++;
+          }
+          } */
+    }
 ?>
 
