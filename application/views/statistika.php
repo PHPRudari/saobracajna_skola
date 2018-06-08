c3e924500b55fae509a5b6a04ef14dbdeff95577
DraganBozic <bzcdrgn@gmail.com>
2018-06-08 11:19:24 +0200

    qwerty

--- application/views/statistika.php
+++ application/views/statistika.php
@@ -5,58 +5,57 @@
 <a  href="</?php echo site_url($controller . "/stampa/prijavljeni") ?>">Штампај пријављене испите</a>-->
 
 <div class="row">
-    
-    <div class="form-group row form-inline" col-md-4>
-        <label class="col-md-4 col-form-label" for="godina_prijave">Година:</label>
-    <?php echo form_open($controller . '/pregled_prijava'); ?>
-    <br>
-     <select name="godina_prijave" class="form-control ">
-    <option selected hidden>Година</option>
-    
-            <?php
-            for ($i = date("Y"); $i >= 2010; $i--) {
-                echo "<option value='$i'>";
-                echo $i;
-                echo "</option>";
-            }
-            ?>
-    </select> 
-    </div>
-    <div class="form-group row form-inline col-md-4">
-        <label class="col-md-4 col-form-label" for="rok_prijave">Рок:</label>
-        <br>
-     <select name="rok_prijave">
-    <option selected hidden>Рок</option>
-      
-
-  <?php
-        foreach ($_SESSION['rok'] as $rok) {
-            echo "<option value='".$rok['idtip_roka']."'".">";
-                echo $rok['naziv'];
-                echo "</option>";
-                
-                
-        }         
+    <div col-md-6 class="form-group row form-inline">
         
-                    
-                    ?>
-    
-     </select>
+            <label class="col-sm-4 col-form-label" for="godina_prijave">Година:</label>
+            <?php echo form_open($controller . '/pregled_prijava'); ?>
+            <br>
+            <select name="godina_prijave" class="form-control ">
+                <option selected hidden>Година</option>
+
+                <?php
+                for ($i = date("Y"); $i >= 2010; $i--) {
+                    echo "<option value='$i'>";
+                    echo $i;
+                    echo "</option>";
+                }
+                ?>
+            </select> 
+        
+    </div>
+    <div col-md-6 class="form-group row form-inline">
+        
+            <label class="col-sm-4 col-form-label" for="rok_prijave">Рок:</label>
+            <br>
+            <select name="rok_prijave" class="form-control ">
+                <option selected hidden>Рок</option>
+
+
+                <?php
+                foreach ($_SESSION['rok'] as $rok) {
+                    echo "<option value='" . $rok['idtip_roka'] . "'" . ">";
+                    echo $rok['naziv'];
+                    echo "</option>";
+                }
+                ?>
+
+            </select>
+        
     </div>
 </div>
 
 <div class="row">
-    
+
     <div class="col-md-6 ">  
         <br>
-    <input class="dugme1 btn btn-primary btn-lg btn-block" type="submit" value="Прикажи испите">
+        <input class="dugme1 btn btn-primary btn-lg btn-block" type="submit" value="Прикажи испите">
     </div> 
     <div class="col-md-6 ">
-        
+
     </div>
 </form>
 
 </div>
-    
+
 
 
