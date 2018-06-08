8fde1d17c40438e1100bde1a0bf76b439b9b7eb4
DraganBozic <bzcdrgn@gmail.com>
2018-06-08 11:07:32 +0200

    1

--- application/views/statistika.php
+++ application/views/statistika.php
@@ -6,9 +6,12 @@
 
 
 <div class="row">
+    
+    <div class="form-group row form-inline" col-md-4>
+        <label class="col-md-4 col-form-label" for="godina_prijave">Година:</label>
     <?php echo form_open($controller . '/pregled_prijava'); ?>
     <br>
-     <select name="godina_prijave">
+     <select name="godina_prijave" class="form-control ">
     <option selected hidden>Година</option>
     
             <?php
@@ -19,7 +22,10 @@
             }
             ?>
     </select> 
-    
+    </div>
+    <div class="form-group row form-inline col-md-4">
+        <label class="col-md-4 col-form-label" for="rok_prijave">Рок:</label>
+        <br>
      <select name="rok_prijave">
     <option selected hidden>Рок</option>
       
@@ -37,11 +43,21 @@
                     ?>
     
      </select>
-    
-    <input type="submit" value="Прикажи испите">
-    
-</form>
-    
-    
+    </div>
 </div>
+
+<div class="row">
+    
+    <div class="col-md-6 ">  
+        <br>
+    <input class="dugme1 btn btn-primary btn-lg btn-block" type="submit" value="Прикажи испите">
+    </div> 
+    <div class="col-md-6 ">
+        
+    </div>
+</form>
+
+</div>
+    
+
 
