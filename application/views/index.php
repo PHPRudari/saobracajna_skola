f469e2c863b24aba67fb79933893d41119286d48
Dragoljub Djordjevic <dragoljubd@gmail.com>
2018-06-10 08:52:37 +0200

    2 izmene

--- application/views/index.php
+++ application/views/index.php
@@ -1,3 +1,4 @@
+<div class="cont-poz">
 <?php
 
  $name= $this->session->userdata('ime');
@@ -6,3 +7,8 @@
  echo "<h1>Саобраћајно-техничке школе у Земуну! </h1>"."<br><br><br>";
  /*echo "Улогован је корисник ".$name;
  echo "<br>".$tip;*/
+ ?>
+
+ <!--<img  classs="index_pozadina" src="<?php echo base_url("slike/pozadina_logovanje.jpg"); ?>" />-->
+    
+</div>  
